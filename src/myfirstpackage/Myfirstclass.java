81d3a127d6d7ed472339a7aad21ca4a73734eec2
alimuzzaman408 <alimuzzaman408@gmail.com>
2019-09-10 07:59:35 -0400

    second commit

--- src/myfirstpackage/Myfirstclass.java
+++ src/myfirstpackage/Myfirstclass.java
@@ -4,6 +4,8 @@ public class Myfirstclass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Bangladesh");
+		System.out.println("Hello india");
+		System.out.println("Hello pakistan");
 
 	}
 
